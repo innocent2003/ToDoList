da843de04f8fed24cde12b0596dd4699da82dc2e
innocent2003 <leminhtuanthpttranhungdaotx@gmail.com>
2024-12-14 22:06:29 +0700

    first commit

---
+++ task-list-app/database/seeders/TaskSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            Task::create([
+                'title' => $faker->sentence(),
+                'description' => $faker->paragraph(),
+                'long_description' => $faker->optional()->text(),
+                'completed' => $faker->boolean(),
+            ]);
+        }
+
+    }
+}
